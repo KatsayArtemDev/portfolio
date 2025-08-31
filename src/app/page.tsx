17a44326470569c3a5ae8d0de8b89d02cd656dc8
katsay <katsayartemdev@gmail.com>
2025-08-31 21:48:24 +0300

    made dot notation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 import Footer from "@/components/footer";
-import About from "@/components/section/about";
-import Contacts from "@/components/section/contacts";
-import Experience from "@/components/section/experience";
-import Main from "@/components/section/main";
-import Projects from "@/components/section/projects";
-import Skills from "@/components/section/skills";
-import Study from "@/components/section/studing";
+import { Section } from "@/components/section";
 
 export default function () {
   return (
     <div className="w-full md:max-w-6xl">
-      <Main />
-      <About />
-      <Experience />
-      <Skills />
-      <Study />
-      <Projects />
-      <Contacts />
+      <Section.Main />
+      <Section.About />
+      <Section.Experience />
+      <Section.Skills />
+      <Section.Studing />
+      <Section.Projects />
+      <Section.Contacts />
       <Footer />
     </div>
   );
---
+++ src/components/section/index.tsx
@@ -0,0 +1,17 @@
+import About from "./about";
+import Contacts from "./contacts";
+import Experience from "./experience";
+import Main from "./main";
+import Projects from "./projects";
+import Skills from "./skills";
+import Studing from "./studing";
+
+export const Section = {
+  Main,
+  About,
+  Experience,
+  Skills,
+  Studing,
+  Projects,
+  Contacts,
+};
